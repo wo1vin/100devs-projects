943560f252c01fafaa67e1f7f66024ae69f5c9e5
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2025-02-16 15:56:36 -0500

    added nav with toggle functionality

--- src/main.js
+++ src/main.js
@@ -9,6 +9,33 @@ const client = new Client()
 export const databases = new Databases( client );
 
 
+// navbar button display toggle
+// Get references to the buttons and divs
+const infoBtn = document.getElementById('infoBtn');
+const addProjectBtn = document.getElementById('addProjectBtn');
+const infoDiv = document.getElementById('info');
+const addProjectDiv = document.getElementById('addProject');
+
+// Toggle function for "Learn more" button
+infoBtn.addEventListener('click', () => {
+  if (infoDiv.style.display === 'none' || infoDiv.style.display === '') {
+    infoDiv.style.display = 'block'; // Show the div
+    addProjectDiv.style.display = 'none'; // Hide the other div
+  } else {
+    infoDiv.style.display = 'none'; // Hide the div
+  }
+});
+
+// Toggle function for "Add a project" button
+addProjectBtn.addEventListener('click', () => {
+  if (addProjectDiv.style.display === 'none' || addProjectDiv.style.display === '') {
+    addProjectDiv.style.display = 'block'; // Show the div
+    infoDiv.style.display = 'none'; // Hide the other div
+  } else {
+    addProjectDiv.style.display = 'none'; // Hide the div
+  }
+});
+
 // set credit in footer
 const currentYear = new Date().getFullYear();
 let credit = document.getElementById("credits");
